26b6391a7975834490966063795342246e762357
unknown <taticeschim@hotmail.com>
2019-03-09 19:59:00 -0300

    projeto iniciado

---
+++ primeiroProjeto/ControleParimonio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace primeiroProjeto
+{
+    class ControleParimonio
+    {
+
+        public string Nome { get; private set; }
+        public string MarcaId { get; private set; }
+        public string Descricao { get; set; }
+        public int NTombo { get; private set; }
+
+        public ControleParimonio(string nome, string marcaId, string descricao, int nTombo)
+        {
+            Nome = nome;
+            MarcaId = marcaId;
+            Descricao = descricao;
+            NTombo = nTombo;
+        }
+
+        public override string ToString()
+        {
+            return "Nome do produto: "
+                + Nome
+                + ", Id Marca: "
+                + MarcaId
+                + ", Descrição: $ "
+                + Descricao
+                + ", Numero do Tombo "
+                + NTombo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ primeiroProjeto/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace primeiroProjeto
+{
+    class Program
+    {
+        private static readonly int nTombo;
+
+        static void Main(string[] args)
+        {
+            ControleParimonio controle;
+                  //  double ntombo = 0;
+
+            Console.Write("deseja incluir um patrimonio (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            int ntombo;
+            ntombo = 0;
+            if (resp == 's' || resp == 'S')
+            {
+                do
+                {
+                    ntombo++;
+                    Console.Write("Entre com o nome do produto: ");
+                    string nome = Console.ReadLine();
+                    Console.Write("Entre com a marca do produto: ");
+                    string marcaId = Console.ReadLine();
+                    Console.Write("Entre com a descrição do produto: ");
+                    string descricao = Console.ReadLine();
+                    controle = new ControleParimonio(nome, marcaId, descricao, nTombo);
+                    Console.Write("deseja incluir um patrimonio (s/n)? ");
+                    resp = char.Parse(Console.ReadLine());
+                }
+                while (resp == 's' || resp == 'S');
+            }
+            else
+            {
+                Console.WriteLine("Obrigada!");
+            }
+        }
+    }
+}
